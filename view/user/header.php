b8768adf7206c4ada9cedc6f26a6b43b934e1c2e
fedgds <sangokyes@gmail.com>
2023-11-04 21:29:53 +0700

    message

---
+++ view/user/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2 SPORT</title>
+    <link rel="icon" type="image/png" href="../../assets/img/brand.png">
+    <link rel="stylesheet" href="../../assets/fontawesome-free-6.4.0-web/css/all.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo-header">
+                <img src="../../assets/img/logo.png" alt="">
+            </div>
+            <div class="form-search">
+                <form action="">
+                    <input type="text" placeholder="Bạn cần tìm gì...">
+                    <button><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <ul>
+                            <li><a href="">Hà nội</a></li>
+                            <li><a href="">Đà nẵng</a></li>
+                            <li><a href="">TP.Hồ chí minh</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Check đơn hàng</a></li>
+                    <li><a href="">Giỏ hàng</a></li>
+                    <li><a href="">Tài khoản</a></li>
+                    <li>
+                        <ul>
+                            <li><a href=""></a></li>
+                            <li><a href=""></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </header>
