ceb711f548625adf556702ce2d071484ce54b56e
fedgds <sangokyes@gmail.com>
2023-11-05 21:47:04 +0700

    nothing

--- view/user/header.php
+++ view/user/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>2 SPORT</title>
     <link rel="icon" type="image/png" href="../../assets/img/brand.png">
-    <link rel="stylesheet" href="../../assets/fontawesome-free-6.4.0-web/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class="container">
