ad285cb48b66aa9caa41617640e9776b9abddc7d
fedgds <sangokyes@gmail.com>
2023-11-08 21:10:57 +0700

    update

--- view/user/home.php
+++ view/user/home.php
@@ -4,8 +4,39 @@
         <button class="pre" onclick="prev()">&#10094;</button>
         <button class="next" onclick="next()">&#10095;</button>
     </div>
-    <div class="">
-        
+    <div class="catalog">
+        <p>DANH MỤC</p>
+        <P>SALE KHỦNG GIẢM LÊN TỚI 50% KHI ĐẶT HÀNG TRÊN 2 TRIỆU ĐỒNG</P>
+        <div class="list-catalog">
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/giaythethao.png" alt=""></a>
+                <a href="">Giày thể thao</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/gangtaythumon.png" alt=""></a>
+                <a href="">Găng tay thủ môn</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/quanaobongda.png" alt=""></a>
+                <a href="">Quần áo bóng đá</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/quabongda.png" alt=""></a>
+                <a href="">Quả bóng đá</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/tatbongdangan.png" alt=""></a>
+                <a href="">Tất bóng đá ngắn</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/tatbongdadai.png" alt=""></a>
+                <a href="">Tất bóng đá dài</a>
+            </div>
+            <div class="product-symbolic">
+                <a href=""><img src="assets/img/tuidunggiay.png" alt=""></a>
+                <a href="">Túi đựng giày</a>
+            </div>
+        </div>
     </div>
 </main>
     
