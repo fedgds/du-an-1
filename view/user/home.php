594d65f8034b23a40aa584251a53c7b151826f6c
fedgds <sangokyes@gmail.com>
2023-11-08 22:20:01 +0700

    update

--- view/user/home.php
+++ view/user/home.php
@@ -5,36 +5,38 @@
         <button class="next" onclick="next()">&#10095;</button>
     </div>
     <div class="catalog">
-        <p>DANH MỤC</p>
-        <P>SALE KHỦNG GIẢM LÊN TỚI 50% KHI ĐẶT HÀNG TRÊN 2 TRIỆU ĐỒNG</P>
+        <div class="text">
+            <a href="">DANH MỤC</a>
+            <P>SALE KHỦNG GIẢM LÊN TỚI 50% KHI ĐẶT HÀNG TRÊN 2 TRIỆU ĐỒNG</P>
+        </div>
         <div class="list-catalog">
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/giaythethao.png" alt=""></a>
-                <a href="">Giày thể thao</a>
+                <a class="name-catalog" href="">GIÀY THỂ THAO</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/gangtaythumon.png" alt=""></a>
-                <a href="">Găng tay thủ môn</a>
+                <a class="name-catalog" href="">GĂNG TAY THỦ MÔN</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/quanaobongda.png" alt=""></a>
-                <a href="">Quần áo bóng đá</a>
+                <a class="name-catalog" href="">QUẦN ÁO BÓNG ĐÁ</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/quabongda.png" alt=""></a>
-                <a href="">Quả bóng đá</a>
+                <a class="name-catalog" href="">QUẢ BÓNG ĐÁ</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/tatbongdangan.png" alt=""></a>
-                <a href="">Tất bóng đá ngắn</a>
+                <a class="name-catalog" href="">TẤT BÓNG ĐÁ NGẮN</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/tatbongdadai.png" alt=""></a>
-                <a href="">Tất bóng đá dài</a>
+                <a class="name-catalog" href="">TẤT BÓNG ĐÁ DÀI</a>
             </div>
             <div class="product-symbolic">
                 <a href=""><img src="assets/img/tuidunggiay.png" alt=""></a>
-                <a href="">Túi đựng giày</a>
+                <a class="name-catalog" href="">TÚI ĐỰNG GIÀY</a>
             </div>
         </div>
     </div>
