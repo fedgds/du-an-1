938fd0bc101618337393b6ceff4998e9cb98df3c
fedgds <sangokyes@gmail.com>
2023-11-10 11:15:52 +0700

    update

---
+++ view/user/dangky.php
@@ -0,0 +1,14 @@
+<div class="form-signup">
+    <h1>ĐĂNG KÝ</h1>
+    <form action="">
+        <input type="text" placeholder="Vui lòng nhập số điện thoại/email">
+        <input type="text" placeholder="Vui lòng nhập họ tên">
+        <input type="password" placeholder="Vui lòng nhập mật khẩu">
+        <input type="password" placeholder="Vui lòng nhập lại mật khẩu">
+        <div class="accept">
+            <input type="checkbox"><label for="">Tôi đồng ý với các điều khoản bảo mật cá nhân</label>
+        </div>
+        <input type="button" value="Đăng ký">
+        <p>Bạn đã có tài khoản?<a class="signin-now" href="">Đăng nhập ngay</a></p>
+    </form>
+</div>
